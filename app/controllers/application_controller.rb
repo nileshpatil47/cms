bb56b6ac0464d98ec85dda41a057ed76c84385ce
Nilesh Patil <nileshpatil47@yahoo.com>
2014-04-29 18:11:09 +0530

    devise changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  layout :layout_by_resource
   helper_method :resource, :resource_name, :devise_mapping
   def resource_name
     :user
@@ -11,5 +12,13 @@ class ApplicationController < ActionController::Base
 
   def devise_mapping
     @devise_mapping ||= Devise.mappings[:user]
+  end
+
+  def layout_by_resource
+    if devise_controller?# && resource_name == :user && action_name == 'sign_in'
+      "content"
+    else
+      "application"
+    end
   end
 end
